4bede5d71a6090be756dbf8db62c210dfcc421e2
joseph-mathias <nchimunya882@gmail.com>
2022-07-04 15:12:11 +0200

    complete

--- BulkyBooksWeb/Controllers/CategoryController.cs
+++ BulkyBooksWeb/Controllers/CategoryController.cs
@@ -17,4 +17,85 @@ public class CategoryController : Controller
         // var objCategoryList = _db.Categories.ToList();
         return View(objCategoryList);
     }
+    // GET
+    public IActionResult Create()
+    {
+        IEnumerable<Category> objCategoryList = _db.Categories;
+        // var objCategoryList = _db.Categories.ToList();
+        return View();
+    }
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Create(Category obj)
+    {
+        if(obj.Name == obj.DisplayOrder.ToString()){
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+        }
+        if(ModelState.IsValid){
+            _db.Categories.Add(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category created successfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+    public IActionResult Edit(int? id)
+    {
+        if(id == null || id == 0){
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(id);
+        // var categoryFromDb = _db.Categories.FindOrDefault(id);
+        // var categoryFromDb = _db.Categories.SinglrOrDefault(id);
+
+        if(categoryFromDb == null){
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+    // POST
+    [HttpPost]
+    [ValidateAntiForgeryToken]
+    public IActionResult Edit(Category obj)
+    {
+        if(obj.Name == obj.DisplayOrder.ToString()){
+            ModelState.AddModelError("name", "The DisplayOrder cannot exactly match the Name");
+        }
+        if(ModelState.IsValid){
+            _db.Categories.Update(obj);
+            _db.SaveChanges();
+            TempData["success"] = "Category updated successfully";
+            return RedirectToAction("Index");
+        }
+        return View(obj);
+    }
+    public IActionResult Delete(int? id)
+    {
+        if(id == null || id == 0){
+            return NotFound();
+        }
+        var categoryFromDb = _db.Categories.Find(id);
+        // var categoryFromDb = _db.Categories.FindOrDefault(id);
+        // var categoryFromDb = _db.Categories.SinglrOrDefault(id);
+
+        if(categoryFromDb == null){
+            return NotFound();
+        }
+        return View(categoryFromDb);
+    }
+    // POST
+    [HttpPost, ActionName("Delete")]
+    [ValidateAntiForgeryToken]
+    public IActionResult DeletePost(int? id)
+    {
+        var obj= _db.Categories.Find(id);
+        if(obj == null){
+            return NotFound();
+        }
+        _db.Categories.Remove(obj);
+        _db.SaveChanges();
+        TempData["success"] = "Category deleted successfully";
+        return RedirectToAction("Index");
+    }
 }
--- BulkyBooksWeb/Models/Category.cs
+++ BulkyBooksWeb/Models/Category.cs
@@ -1,4 +1,5 @@
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
 
 namespace BulkyBooksWeb.Models;
 public class Category
@@ -7,6 +8,8 @@ public class Category
     public int id { get; set; }
     [Required]
     public string Name { get; set; }
+    [DisplayName("Display Order")]
+    [Range(1, 100, ErrorMessage = "Display Order must be between 1 and 100 only!")]
     public int DisplayOrder { get; set; }
     public DateTime CreatedDateTime { get; set; } = DateTime.Now;
 }
